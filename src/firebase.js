fa958b027b3310f561129b89343a47a738473522
Pranay Khojare <khojarepranay6@gmail.com>
2025-09-30 16:35:40 +0530

    Updated Netflix Clone with Firebase env config

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIcwtY63Gq9l79rntzvTnl302WIyWpVaE",
-  authDomain: "netflix-clone-20d11.firebaseapp.com",
-  projectId: "netflix-clone-20d11",
-  storageBucket: "netflix-clone-20d11.firebasestorage.app",
-  messagingSenderId: "893023146389",
-  appId: "1:893023146389:web:1e58034a0e1fb4afbea69c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
